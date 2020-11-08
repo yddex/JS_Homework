a010a735389e6419570ee667bae84bac225cea4d
Ydee <47116192+yddex@users.noreply.github.com>
2020-11-09 01:06:06 +0300

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,18 @@
+function Sum(a,b){
+    return a + b;
+}
+function Vch(a,b){
+    return a - b;
+}
+function Pr(a,b){
+    return a * b;
+}
+function del(a,b){
+    return a / b;
+}
+var a=2,b=2;
+alert("Числа: " + a + " " + b);
+alert("Сумма: " + Sum(a,b));
+alert("Вычитание: " + Vch(a,b));
+alert("Произведение: " + Pr(a,b));
+alert("Делениe: " + del(a,b));
