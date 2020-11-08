02c771624cb44bf38319344f02762029a919e51a
Ydee <47116192+yddex@users.noreply.github.com>
2020-11-09 00:58:20 +0300

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,38 @@
+var a;
+a = parseInt(Math.random() * 15);
+
+switch(a){
+    case 1: document.write("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15");
+            break;
+    case 2: document.write("2,3,4,5,6,7,8,9,10,11,12,13,14,15");
+            break;
+    case 3: document.write("3,4,5,6,7,8,9,10,11,12,13,14,15");
+            break;
+    case 4: document.write("4,5,6,7,8,9,10,11,12,13,14,15");
+            break;
+    case 5: document.write("5,6,7,8,9,10,11,12,13,14,15");
+            break;
+    case 6: document.write("6,7,8,9,10,11,12,13,14,15");
+            break;
+    case 7: document.write("7,8,9,10,11,12,13,14,15");
+            break;
+    case 8: document.write("8,9,10,11,12,13,14,15");
+            break;
+    case 9: document.write("9,10,11,12,13,14,15");
+            break;
+    case 10: document.write("10,11,12,13,14,15");
+            break;
+    case 11: document.write("11,12,13,14,15");
+            break;
+    case 12: document.write("12,13,14,15");
+            break;
+    case 13: document.write("13,14,15");
+            break;
+    case 14: document.write("14,15");
+            break;
+    case 15: document.write("15");
+            break;
+    default: document.write("Произошла ошибка")
+            break;
+
+}
