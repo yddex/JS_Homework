c4864e91a7fe8e9d710650b5a419b1218ed10b6b
Ydee <47116192+yddex@users.noreply.github.com>
2020-11-16 21:13:56 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,19 @@
+<script>
+function NumObj(number){
+    var correct = false;
+    
+    if(number>999 || isNaN(number) || number<0){
+        alert("Введите число от 0 до 999")
+    }
+    else{
+        this.sot=parseInt(number/100);
+        this.des=parseInt((number-this.sot*100)/10);
+        this.ed=parseInt(number-this.sot*100-this.des*10);
+        correct = true;
+    }
+
+}
+var num = +prompt("Введите число от 0 до 999:");
+var obj = new NumObj(num);
+console.log("Сотни: "+obj.sot+"\nесятки: "+obj.des+"\nЕдиницы: "+obj.ed);
+</script>
