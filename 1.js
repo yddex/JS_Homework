0fd19480eb9512f19fb479e54ee5b4622c8fc650
Ydee <47116192+yddex@users.noreply.github.com>
2020-11-21 15:55:14 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -8,7 +8,7 @@ function NumObj(number){
     else{
         this.sot=parseInt(number/100);
         this.des=parseInt((number%100)/10);
-        this.ed=parseInt(number%10);
+        this.ed=Math.floor(number%10);
         correct = true;
     }
 
